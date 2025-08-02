d4b456f2a235e559ba8fbf90b187060b75d99f14
Abraham King <abe.king@gmail.com>
2025-08-02 22:28:39 +0000

    the following error is persisting during the npm run build process: .nex

--- src/app/bird/[birdId]/page.tsx
+++ src/app/bird/[birdId]/page.tsx
@@ -1,11 +1,6 @@
 
 import { FalconryJournalClient } from "@/components/falconry-journal-client";
 
-interface PageProps {
-  params: { birdId: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default async function BirdPage({ params }: PageProps) {
+export default async function BirdPage({ params }: { params: { birdId: string } }) {
   return <FalconryJournalClient view="detail" selectedBirdId={params.birdId} />;
 }
