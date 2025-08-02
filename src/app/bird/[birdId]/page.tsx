9b0bcdf138ada1a823bb686ad082b41ac5ec0fb2
Abraham King <abe.king@gmail.com>
2025-08-02 23:22:30 +0000

    wrapping ID objects with Promise

--- src/app/bird/[birdId]/page.tsx
+++ src/app/bird/[birdId]/page.tsx
@@ -1,6 +1,11 @@
 
 import { FalconryJournalClient } from "@/components/falconry-journal-client";
 
-export default async function BirdPage({ params }: { params: { birdId: string } }) {
-  return <FalconryJournalClient view="detail" selectedBirdId={params.birdId} />;
+interface PageProps {
+  params: Promise<{ birdId: string }>;
+}
+
+export default async function BirdPage({ params }: PageProps) {
+  const { birdId } = await params;
+  return <FalconryJournalClient view="detail" selectedBirdId={birdId} />;
 }
