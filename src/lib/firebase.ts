2daaaf43e50b7ec8b6093e074be35f30c2d804a7
Abraham King <abe.king@gmail.com>
2025-08-02 17:30:27 +0000

    add the following code: // Import the functions you need from the SDKs y

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqq2n2IvW9pTr8-oVcTRheIQccdk7Ci7c",
+  authDomain: "falconry-journal.firebaseapp.com",
+  projectId: "falconry-journal",
+  storageBucket: "falconry-journal.appspot.com",
+  messagingSenderId: "383208156265",
+  appId: "1:383208156265:web:102fe908ba808d623746f8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
